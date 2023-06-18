d585e4ca2360247df2a804faf2c15ed797b9193e
RYZEN\tomas <Molekula>
2023-06-18 21:03:39 +0200

    rewrite

--- rtl/peripheral/slots/ascii8.sv
+++ rtl/peripheral/slots/ascii8.sv
@@ -24,31 +24,14 @@ wire [1:0] region  = cpu_addr[12:11];
 always @(posedge clk) begin
     if (reset) begin
         bank <= '{'{default: '0},'{default: '0}};
-        //bank0 <= '{'h00,'h00};
-        //bank1 <= '{'h00,'h00};
-        //bank2 <= '{'h00,'h00};
-        //bank3 <= '{'h00,'h00};
     end else begin
-        if (cs & cpu_mreq & cpu_wr) begin
-            case (cpu_addr[15:11])
-                5'b01100: // 6000-67ffh
-                    bank[cart_num][0] <= din;
-                5'b01101: // 6800-6fffh
-                    bank[cart_num][1] <= din;
-                5'b01110: // 7000-77ffh
-                    bank[cart_num][2] <= din;
-                5'b01111: // 7800-7fffh
-                    bank[cart_num][3] <= din;
-                default: ;
-            endcase
+        if (cs & cpu_mreq & cpu_wr & cpu_addr[15:13] == 3'b011) begin
+            bank[cart_num][cpu_addr[12:11]] <= din;     
         end
     end
 end
 
-assign bank_base = cpu_addr[14:13] == 2'b00 ? bank[cart_num][2] : 
-                   cpu_addr[14:13] == 2'b01 ? bank[cart_num][3] : 
-                   cpu_addr[14:13] == 2'b10 ? bank[cart_num][0] : 
-                                          bank[cart_num][1] ;
+assign bank_base = bank[cart_num][{cpu_addr[15],cpu_addr[13]}];                  
 
 assign mem_addr = 25'({bank_base, cpu_addr[12:0]});
 assign mem_unmaped = cs & ((mem_addr > rom_size) | ~^cpu_addr[15:14]);
